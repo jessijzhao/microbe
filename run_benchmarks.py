eb8bf760c90c6717f51eae306144d50c183ce04f
jessijzhao <jessicazhao@fb.com>
2021-12-03 01:44:08 +0000

    Repeat runs for multiple inputs, report mean and std

--- run_benchmarks.py
+++ run_benchmarks.py
@@ -1,4 +1,5 @@
 import logging
+import copy
 import torch
 from layers import LayerFactory
 from benchmarks import BenchmarkFactory
@@ -34,41 +35,58 @@ def main() -> None:
 
             for comparison in layer_set["comparison"]:
                 assert(len(comparison) == 2)
-                print(f"Comparing {comparison} ...")
+                print(f"Comparing {comparison} ...\n")
 
                 results = pd.DataFrame(columns=["Forward only", "Forward backward"])
+                std = copy.deepcopy(results)
 
                 for layer_type in comparison:
-                    # setup layer
-                    layer_fun = LayerFactory.create(
-                        layer_type=layer_type,
-                        **layer_set["config"]
-                    )
-                    layer_fun.layer.to(device)
+                    runtimes = []
+                    for _ in range(config["num_repeats"]):
+                        
+                        # setup layer
+                        layer_fun = LayerFactory.create(
+                            layer_type=layer_type,
+                            **layer_set["config"]
+                        )
+                        layer_fun.layer.to(device)
+                        
+                        # benchmark forward_only
+                        with torch.no_grad():
+                            layer_fun.prepare_forward_only()
+                            forward_only_runtime = benchmark_run.run(
+                                function=layer_fun.forward_only_no_hooks,
+                                **conf, 
+                            )
+                        logger.info(f"Runtime for {layer_type} on forward_only: {forward_only_runtime} ms")
+                        
                     
-                    # benchmark forward_only
-                    with torch.no_grad():
-                        layer_fun.prepare_forward_only()
-                        forward_only_runtime = benchmark_run.run(
-                            function=layer_fun.forward_only_no_hooks,
+                        # benchmark forward_backward
+                        layer_fun.prepare_forward_backward()
+                        forward_backward_runtime = benchmark_run.run(
+                            function=layer_fun.forward_backward,
                             **conf, 
                         )
-                    logger.info(f"Runtime for {layer_type} on forward_only: {forward_only_runtime} ms")
-                    
-                
-                    # benchmark forward_backward
-                    layer_fun.prepare_forward_backward()
-                    forward_backward_runtime = benchmark_run.run(
-                        function=layer_fun.forward_backward,
-                        **conf, 
-                    )
-                    logger.info(f"Runtime for {layer_type} on forward_backward: {forward_backward_runtime} ms")
-                    
-                    del layer_fun
+                        logger.info(f"Runtime for {layer_type} on forward_backward: {forward_backward_runtime} ms")
+                        
+                        del layer_fun
+                        runtimes.append((forward_only_runtime, forward_backward_runtime))
 
+                    runtimes = np.array(runtimes)
+                    forward_only_mean, forward_backward_mean = np.mean(runtimes, axis=0)
+                    forward_only_std, forward_backward_std = np.std(runtimes, axis=0)
+                     
                     results = results.append(
                         pd.Series(
-                            [forward_only_runtime, forward_backward_runtime],
+                            [forward_only_mean, forward_backward_mean],
+                            name=layer_type,
+                            index=results.columns
+                        )
+                    )
+                 
+                    std = std.append(
+                        pd.Series(
+                            [forward_only_std, forward_backward_std],
                             name=layer_type,
                             index=results.columns
                         )
@@ -85,7 +103,11 @@ def main() -> None:
                         index=results.columns
                     )
                 )
+                print(f"Mean runtime across {config['num_repeats']} runs:")
                 print(results, "\n")
+
+                print(f"Standard deviation across {config['num_repeats']} runs:")
+                print(std, "\n")
                 
    
 if __name__ == "__main__":
