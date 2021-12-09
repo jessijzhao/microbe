Fix memory leakage

layers.py
@@ -66,7 +66,7 @@ class LayerFactory:
             preds = self.forward_only()
             loss = self.criterion(preds, self.labels)
             loss.backward()
-
+            self.layer.zero_grad()
 
     class LinearBase(Layer):
         def __init__(
@@ -247,10 +247,8 @@ class LayerFactory:
             self.labels = torch.randn(batch_size, *input_shape, embedding_dim, device=device)
     
     class CLayer(Layer):
-        def forward_backward(self) -> torch.Tensor:
-            preds, _ = self.forward_only()
-            loss = F.cross_entropy(preds, self.labels)
-            loss.backward()
+        def forward_only(self):
+            return self.layer(*self.layer_inputs)[0]
 
     class MHABase(CLayer):
         def __init__(
@@ -310,8 +308,7 @@ class LayerFactory:
                     targ_seq_len, batch_size, embed_dim, device=device
                 ) if not batch_first
                 else torch.randn(batch_size, targ_seq_len, embed_dim, device=device)
-            )            
-    
+            )
 
     class RNNBase(CLayer):
         def __init__(
@@ -365,7 +362,7 @@ class LayerFactory:
                     batch_size, seq_len, self.D * hidden_size, device=device
                 )
             )
-
+        
 
     class LSTMBase(RNNBase):
         def __init__(
@@ -411,6 +408,7 @@ class LayerFactory:
                 ) if not batch_first
                 else torch.randn(batch_size, seq_len, self.D * h_out, device=device)
             )
+
             
 
     def make_private(layer: Layer) -> Layer:

run_benchmarks.py
@@ -3,8 +3,9 @@ import logging
 import copy
 import torch
 import pickle
-from layers import LayerFactory
+from layers import LayerType, LayerFactory
 from benchmarks import BenchmarkFactory
+from os.path import exists
 import json
 import torch.nn as nn
 import pandas as pd
@@ -12,7 +13,10 @@ import numpy as np
 
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.WARNING)
+
+def get_path(layer_name, batch_size, config):
+    pickle_name = f"{layer_name}_bs_{batch_size}_runs_{config['num_runs']}_repeats_{config['num_repeats']}"
+    return f'./results/raw/{pickle_name}.pkl'
 
 def save_results(layer_name, batch_size, config, runtimes, memory):
     data = {
@@ -22,11 +26,10 @@ def save_results(layer_name, batch_size, config, runtimes, memory):
         'runtimes': runtimes,
         'memory': memory
     }
-    pickle_name = f"{layer_name}_bs_{batch_size}_runs_{config['num_repeats']}_repeats_{config['num_runs']}"
-    full_path = './results/raw/' + pickle_name + '.pkl'
-    print('Saving to: ', full_path)
+    path = get_path(layer_name, batch_size, config)
+    logger.info(f'Saving to: {path}')
 
-    with open(full_path, 'wb') as handle:
+    with open(path, 'wb') as handle:
         pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
@@ -41,72 +44,100 @@ def main() -> None:
         type=str,
         default="config.json"
     )
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        action="store_true"
+    parser.add_argument("-v", "--verbose", action="store_true")
+    parser.add_argument("--timer", default="torch", help="use Python, CUDA or PyTorch timer")
+    parser.add_argument("--num_runs", default=100, type=int, help="number of benchmarking runs")
+    parser.add_argument("--num_repeats", default=20, type=int, help="how many forward/backward passes per run")
+    parser.add_argument("--num_warmups", default=10, type=int, help="number of warmups for custom benchmarks")
+    parser.add_argument('--cont', action="store_true", help="only run missing experiments")
+    parser.add_argument('--batch_sizes', default=[16, 32, 64, 128, 256, 512, 1024, 2048], nargs='+', type=int)
+    parser.add_argument('--layers',
+        default=[v for k, v in LayerType.__dict__.items() if not k.startswith('__')],
+        nargs='+'
     )
+    parser.add_argument('--no_save', action="store_true")
     args = parser.parse_args()
+    
+    # activate logging
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
     
     logger.info(f"Using {args.config_file} as config file.")
     with open(args.config_file) as config_file:
         config = json.load(config_file)
 
     conf = {
-        "num_warmups": config["num_warmups"],
-        "num_repeats": config["num_repeats"],
+        "num_warmups": args.num_warmups,
+        "num_repeats": args.num_repeats,
     }
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     torch.cuda.synchronize()
 
-    benchmark_run = BenchmarkFactory.create(config["benchmark"])
+    benchmark_run = BenchmarkFactory.create(args.timer)
 
-    for layer_set in config["layers"]:
-        if args.verbose:
-            print(json.dumps(layer_set["config"], indent=4))
-
-        for batch_size in config["batch_sizes"]:
+    for batch_size in args.batch_sizes:
+        for layer_set in config["layers"]:
             for layer_name in layer_set["comparison"]:
-                runtimes = []
-                memory = []
-                for _ in range(config["num_runs"]):
+                temp_conf = {
+                    **layer_set["config"],
+                    **conf,
+                    "num_runs": args.num_runs,
+                }
 
-                    # setup layer
-                    layer_fun = LayerFactory.create(
-                        layer_name=layer_name,
-                        batch_size=batch_size,
-                        **layer_set["config"]
-                    )
-                   
-                    try:
-                        # benchmark forward_backward
-                        layer_fun.prepare_forward_backward()
-                        runtime, max_memory = benchmark_run.run(
-                            function=layer_fun.forward_backward,
-                            **conf
+                # skip if we already have the relevant results
+                if (
+                    layer_name not in args.layers
+                    or (args.cont and exists(get_path(layer_name, batch_size, temp_conf)))
+                ):
+                    if layer_name in args.layers:
+                        logger.info(f'Skipping {layer_name} at {batch_size}.')
+
+                else:
+                    runtimes = []
+                    memory = []
+                    for _ in range(args.num_runs):
+                        torch.cuda.empty_cache()
+                        base_mem = torch.cuda.memory_allocated(device) * 1e-9
+                        print('Base memory:', base_mem)
+
+                        # setup layer
+                        layer_fun = LayerFactory.create(
+                            layer_name=layer_name,
+                            batch_size=batch_size,
+                            **layer_set["config"]
                         )
+                       
+                        try:
+                            # benchmark forward_backward
+                            layer_fun.prepare_forward_backward()
+                            runtime, max_memory = benchmark_run.run(
+                                function=layer_fun.forward_backward,
+                                **conf
+                            )
+
+                        except RuntimeError as e: # OOM
+                            runtime  = float('nan')
+                            max_memory = float('nan')
+                            logger.debug(f'{layer_name} at {batch_size} failed with {e}.')
+
                         del layer_fun
                         torch.cuda.empty_cache()
+                        
+                        runtimes.append(runtime)
+                        memory.append(max_memory-base_mem)
 
-                    except RuntimeError: # OOM
-                        runtime  = float('nan')
-                        max_memory = float('nan')
+                        logger.debug(f"Runtime: {runtime}, Memory: {max_memory}")
 
-                    runtimes.append(runtime)
-                    memory.append(max_memory)
-
-                save_results(
-                    layer_name=layer_name,
-                    batch_size=batch_size, 
-                    config={
-                        **layer_set["config"],
-                        **conf,
-                        "num_runs": config['num_runs']
-                    },
-                    runtimes=runtimes,
-                    memory=memory
-                )
+                    logger.debug(f"Mean runtime: {np.mean(runtimes)}, Mean memory: {np.mean(memory)}")
+                    if not args.no_save:
+                        save_results(
+                            layer_name=layer_name,
+                            batch_size=batch_size, 
+                            config=temp_conf,
+                            runtimes=runtimes,
+                            memory=memory
+                        )
 
    
 if __name__ == "__main__":